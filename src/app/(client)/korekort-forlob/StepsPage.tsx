b72ff1be14426c41188f853b7ff3f57aa710d91e
Marc Made It <spurdo.dk@gmail.com>
2025-07-11 10:46:57 +0200

    Update StepsPage.tsx

--- src/app/(client)/korekort-forlob/StepsPage.tsx
+++ src/app/(client)/korekort-forlob/StepsPage.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import React from "react";
-import { useTranslation } from "react-i18next";
 import StepsMenu from "@/components/client/steps/StepsMenu";
 import StepsContent from "@/components/client/steps/StepsContent";
 
 const StepsPage = () => {
-  const { t } = useTranslation();
   return (
     <div className="flex justify-center px-4 md:px-12">
       <div className="flex gap-5 w-full max-w-7xl">
