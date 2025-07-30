6d9dbb0b20835dd0850f307b70225725ef38d585
Marc Made It <spurdo.dk@gmail.com>
2025-07-31 00:15:35 +0200

    update

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,12 @@
-// import Overview from "@/components/admin/overview/Overview";
+import Overview from "@/components/admin/overview/Overview";
 import React from "react";
 
 const AdminPage = () => {
-  return <div className="relative">{/* <Overview /> */}</div>;
+  return (
+    <div className="relative">
+      <Overview />
+    </div>
+  );
 };
 
 export default AdminPage;
