ddd89dd47fe710fd770da55c8e647a1cfe897f43
Marc Made It <spurdo.dk@gmail.com>
2025-08-01 11:44:04 +0200

    update

--- src/components/client/home/CTA.tsx
+++ src/components/client/home/CTA.tsx
@@ -7,11 +7,13 @@ const CTA = () => {
 
   return (
     <section className="w-full py-20 bg-base-100 flex flex-col gap-5 items-center justify-center px-4">
-      <div className="flex flex-col items-center">
-        <h2 className="text-2xl md:text-4xl font-bold mb-4">
+      <div className="flex flex-col items-center text-center">
+        <h2 className="text-xl md:text-4xl font-bold mb-4">
           {t("cta.heading")}
         </h2>
-        <p className="md:text-lg max-w-xl text-center">{t("cta.text")}</p>
+        <p className="text-sm md:text-lg max-w-xl text-center">
+          {t("cta.text")}
+        </p>
       </div>
       <div className="flex items-center flex-col-reverse sm:flex-row gap-4 justify-center">
         <Link href="/tilmelding" className="btn btn-primary md:btn-lg">
