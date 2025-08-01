a5f58ba11da0ab60ea01ad62edd2ef6229783326
Marc Made It <spurdo.dk@gmail.com>
2025-08-01 22:10:09 +0200

    Update page.tsx

--- src/app/(client)/kontakt/page.tsx
+++ src/app/(client)/kontakt/page.tsx
@@ -4,7 +4,7 @@ import ContactPage from "./ContactPage";
 export const metadata: Metadata = {
   title: "Kontakt os",
   description:
-    "Kontakt Junkers Køreskole for at få svar på dine spørgsmål om kørekort, trailer, traktor og generhvervelse. Vi er her for at hjælpe dig med at komme godt i gang med din køreuddannelse",
+    "Kontakt Junkers Køreskole for at få svar på dine spørgsmål om kørekort, trailer, traktor og generhvervelse. Vi er her for at hjælpe dig med at komme godt i gang med din køreuddannelse.",
 };
 
 export default function Page() {
