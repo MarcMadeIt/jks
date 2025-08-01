5553cf953a77e9433adbf9a8c8093268c6bad976
Marc Made It <spurdo.dk@gmail.com>
2025-08-01 22:56:48 +0200

    update

--- src/app/(client)/priser/PricesPage.tsx
+++ src/app/(client)/priser/PricesPage.tsx
@@ -8,13 +8,13 @@ import PricesCards from "@/components/client/home/PricesCards";
 const PricesPage = () => {
   const { t } = useTranslation();
   return (
-    <div className="p-5 sm:p-7 w-full h-full flex flex-col gap-0 md:gap-7 lg:gap-15 xl:gap-28 justify-center items-center relative my-7 md:my-20">
+    <div className="p-5 sm:p-7 w-full h-full flex flex-col justify-center items-center relative my-7 md:my-20">
       <div className="max-w-[290px] md:max-w-[470px]">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold text-center">
           {t("pricesPage.title")}
         </h1>
       </div>
-      <div className="flex flex-col justify-center gap-10 md:gap-15 xl:gap-28 mt-10">
+      <div className="flex flex-col justify-center">
         <PricesCards showTitle={false} />
         <FAQ />
       </div>
